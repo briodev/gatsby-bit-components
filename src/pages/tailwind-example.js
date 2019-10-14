f0c262d6e045c033f40a7b7c3840532c4da257ce
Bruce Berdine <eastcoastgranite.it@gmail.com>
2019-10-14 13:56:26 -0400

    movr tailwind example

--- src/pages/tailwind-example.js
+++ src/pages/tailwind-example.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import Layout from '../layouts/main-layout'
-import TailwindSample from '../components/tailwind-example'
+import TailwindSample from '../components/shared-components/tailwind-example'
 
 export default () => {
   return (
